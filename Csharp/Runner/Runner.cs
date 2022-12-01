ba089527765f127ec7e7b49e9785c89c1cc5731a
zelvaman <honza.rada1@seznam.cz>
2022-12-01 06:42:38 +0100

    work on csharp runner

---
+++ Csharp/Runner/Runner.cs
@@ -0,0 +1,16 @@
+﻿namespace AOC.Runner;
+
+public class Runner
+{
+    public static string RunTask(int day, int year, Arguments.ProblemPart part)
+    {
+        var solution = Type.GetType($"AOC.Year20{year}.Day{day.ToString().PadLeft(2,'0')}");
+        if (solution == null)
+        {
+            throw new Exception("Solution not found");
+        }
+
+        
+        return String.Empty;
+    }
+}
