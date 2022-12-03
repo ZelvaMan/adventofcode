cfa5e7856108ec8bda39280aaea62efca515208e
Jan Rada <31016933+ZelvaMan@users.noreply.github.com>
2022-12-03 15:51:58 +0100

    Add template for days

---
+++ csharp/[TEMPLATE]/YEAR[YEAR]/DAY[DAY]/Solution.cs
@@ -0,0 +1,16 @@
+﻿namespace Csharp.Year[YEAR].Day[DAY];
+
+
+public class Solution
+{
+    public static string Part1(string[] input)
+    {
+        return "not implemented"
+    }
+
+    public static string Part2(string[] input)
+    {
+        return "not implemented"
+
+    }
+}
