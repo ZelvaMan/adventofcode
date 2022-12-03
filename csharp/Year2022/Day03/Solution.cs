0f9bbc35b712b8e0fcfeac4a4a547939ba614c90
Jan Rada <31016933+ZelvaMan@users.noreply.github.com>
2022-12-03 18:27:08 +0100

    csharp 2022/03 reformatting

--- csharp/Year2022/Day03/Solution.cs
+++ csharp/Year2022/Day03/Solution.cs
@@ -6,7 +6,6 @@ public class Solution
     {
         var commonItems = CommonItems(input);
         var sum = commonItems.Select(GetPriority).Sum();
-
         return sum.ToString();
     }
 
@@ -19,34 +18,45 @@ public class Solution
 
     private static List<char> CommonItems(string[] input)
     {
-        var commonItems = input.Select(x =>
-                (x.Substring(0, x.Length / 2),
-                    x.Substring(x.Length / 2, x.Length / 2)))
-            .Select(x => x.Item1.ToHashSet().Intersect(x.Item2.ToHashSet()));
+        //get all common chars between half's of the line 
+        var commonItems = input
+            .Select(x =>
+            (
+                x[..(x.Length / 2)],
+                x.Substring(x.Length / 2, x.Length / 2)
+            ))
+            .Select(x =>
+                x.Item1.ToHashSet().Intersect(x.Item2.ToHashSet()).First()
+            ).ToList();
 
-        return commonItems.Select(x => x.First()).ToList();
+        //only return one, because there is always only one
+        return commonItems;
     }
 
     private static List<char> CommonItemInGroup(string[] input)
     {
-        var commonItems = input.Chunk(3)
-            .Select(x =>
-                x[0].ToHashSet().Intersect(
-                    x[1].ToHashSet().Intersect(
-                        x[2].ToHashSet()
-                    )
-                ).First()
-            ).ToList();
+        var commonItems = input
+            .Chunk(3)
+            .Select(s =>
+            {
+                //get intersecting char between all three strings
+                var result = new HashSet<char>(s[0]);
+                result.IntersectWith(s[1]);
+                result.IntersectWith(s[2]);
+                return result.First();
+            }).ToList();
 
         return commonItems;
     }
 
-    private static int GetPriority(char item)
-    {
-        //UPPERCASE
-        if (item < 97) return item - 38;
-
-        //lovercase
-        return item - 96;
-    }
+    /// <summary>
+    /// for CAPITAL letters subtract 65(A)  and then add 27 = -38
+    /// for non capital subtract 96(A)
+    /// </summary>
+    /// <param name="item"></param>
+    /// <returns></returns>
+    private static int GetPriority(char item) =>
+        item < 97
+            ? item - 38
+            : item - 96;
 }
