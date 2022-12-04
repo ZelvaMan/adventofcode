ccdddc8c2de133339bb9473763a1ef2328fb3874
Jan Rada <31016933+ZelvaMan@users.noreply.github.com>
2022-12-04 13:12:50 +0100

    csharp 2022/04 part 1

---
+++ csharp/Year2022/Day04/Solution.cs
@@ -0,0 +1,60 @@
+﻿using System.Security.Cryptography;
+
+namespace Csharp.Year2022.Day04;
+
+public class Solution
+{
+	public static string Part1(string[] input)
+	{
+		var parsed = input.Select(Range.ParseTwo).ToList();
+
+		int fullyContainerCount = parsed.Where(Range.FullyOverlap).Count();
+		return fullyContainerCount.ToString();
+	}
+
+	public static string Part2(string[] input)
+	{
+		return "not implemented";
+	}
+}
+
+public class Range
+{
+	public long start, end;
+
+	public Range(string line)
+	{
+		var split = line.Split("-");
+		start = int.Parse(split[0]);
+		end = int.Parse(split[1]);
+	}
+
+	public static (Range, Range) ParseTwo(string line)
+	{
+		var split = line.Split(",");
+
+		return (new Range(split[0]), new Range(split[1]));
+	}
+
+	public static bool FullyOverlap((Range r1, Range r2) ranges)
+	{
+		var (r1, r2) = ranges;
+
+		if (r1.IsSubset(r2))
+		{
+			return true;
+		}
+
+		if (r2.IsSubset(r1))
+		{
+			return true;
+		}
+
+		return false;
+	}
+
+	public bool IsSubset(Range r)
+	{
+		return (start <= r.start && end >= r.end);
+	}
+}
