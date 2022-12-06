ac0f867d0ac6dc4e50fdb0ee4c14dc9c57edfbf1
Jan Rada <31016933+ZelvaMan@users.noreply.github.com>
2022-12-06 08:11:09 +0100

    csharp 2022/06 part 1 and 2

---
+++ csharp/Year2022/Day06/Solution.cs
@@ -0,0 +1,46 @@
+﻿namespace Csharp.Year2022.Day06;
+
+
+public class Solution
+{
+    public static string Part1(string[] input)
+    {
+        var packetRaw = input[0];
+        for (var i = 0; i < packetRaw.Length; i++)
+        {
+            var lastFourCharacters = packetRaw.Substring(i, 4);
+
+            //found packet 
+            if (lastFourCharacters.Distinct().Count() == 4)
+            {
+                return (i+4).ToString();
+            }
+                
+            Console.WriteLine(lastFourCharacters);
+        }
+
+
+        return "NO MARKER FOUND";
+    }
+
+    public static string Part2(string[] input)
+    {
+        const int  packetLength = 14;
+        
+        var packetRaw = input[0];
+        for (var i = 0; i < packetRaw.Length; i++)
+        {
+            var lastFourCharacters = packetRaw.Substring(i, packetLength);
+
+            //found packet 
+            if (lastFourCharacters.Distinct().Count() == packetLength)
+            {
+                return (i+packetLength).ToString();
+            }
+        }
+
+
+        return "NO MARKER FOUND";
+
+    }
+}
