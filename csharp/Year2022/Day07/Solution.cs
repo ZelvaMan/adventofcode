856517c56317cf8ef7d902b51733264eedc503f9
Jan Rada <31016933+ZelvaMan@users.noreply.github.com>
2022-12-08 17:12:01 +0100

    csharp 2022/07 refactor

--- csharp/Year2022/Day07/Solution.cs
+++ csharp/Year2022/Day07/Solution.cs
@@ -2,163 +2,156 @@
 
 public class Solution
 {
-    public static string Part1(string[] input)
-    {
-        var fs = new FileSystem(input);
-        Console.WriteLine("TEST");
+	public static string Part1(string[] input)
+	{
+		var fs = new FileSystem(input);
 
-        var dirs = fs.NestedDirectorySize();
+		var dirs = fs.NestedDirectorySize();
 
-        var dirsUndeMil = dirs.Where(x => x.Value < 100000);
-        var sum = dirsUndeMil.Sum(x => x.Value);
-        return sum.ToString();
-    }
+		var dirsUndeMil = dirs.Where(x => x.Value < 100000);
+		var sum = dirsUndeMil.Sum(x => x.Value);
+		
+		return sum.ToString();
+	}
 
-    public static string Part2(string[] input)
-    {
-        const long totalSize = 70000000,updateSize = 30000000;
-        
-        var fs = new FileSystem(input);
-        Console.WriteLine("TEST");
+	public static string Part2(string[] input)
+	{
+		const long totalSize = 70000000, updateSize = 30000000;
 
-        var dirs = fs.NestedDirectorySize();
+		var fs = new FileSystem(input);
+		
+		var dirs = fs.NestedDirectorySize();
 
-        var freeSpace = totalSize - dirs["/"];
-        var spaceNeeded = updateSize - freeSpace;
-
-        var lowestPossible = dirs.Where(x => x.Value > spaceNeeded).OrderBy(x => x.Value).First();
-        
-        
-        return lowestPossible.Value.ToString();
-    }
+		var freeSpace = totalSize - dirs["/"];
+		var spaceNeeded = updateSize - freeSpace;
+		
+		var lowestPossible = dirs.Where(x => x.Value > spaceNeeded).MinBy(x => x.Value);
+		return lowestPossible.Value.ToString();
+	}
 }
 
 public class FileSystem
 {
-    private Dictionary<string, long> Directories = new Dictionary<string, long>();
+	private Dictionary<string, long> Directories = new Dictionary<string, long>();
+	private string cwd = "/";
+
+	public FileSystem(IEnumerable<string> rawInputs)
+	{
+		var lastLs = false;
+
+		var lsOutput = new List<string>();
+		foreach (var line in rawInputs)
+		{
+			//it isnt command, so its part of ls output
+			if (!line.StartsWith("$"))
+			{
+				lsOutput.Add(line);
+				continue;
+			}
 
 
-    public FileSystem(string[] rawInputs)
-    {
-        var cwd = "/";
-        bool lastLs = false;
+			//if it is end of ls command add directory entry
+			if (lastLs)
+			{
+				AddDirectory(cwd, lsOutput);
+			}
 
-        List<string> lines = new List<string>();
+			//do command
+			switch (line[2..4])
+			{
+				case "cd":
+					lastLs = false;
+					ChangeCwd(line);
+					break;
 
+				case "ls":
+					lastLs = true;
+					lsOutput = new List<string>();
+					break;
+			}
+		}
 
-        for (var i = 0; i < rawInputs.Length; i++)
-        {
-            var line = rawInputs[i];
+		//if last command was ls
+		if (lastLs)
+		{
+			AddDirectory(cwd, lsOutput);
+		}
+	}
 
-            //ls entry not command
-            if (!line.StartsWith("$"))
-            {
-                lines.Add(line);
-                continue;
-            }
+	public void ChangeCwd(string command)
+	{
+		var changeTo = command[5..];
+		switch (changeTo)
+		{
+			case "..":
+				cwd = cwd[..cwd.LastIndexOf("/")];
+				if (!cwd.StartsWith("/"))
+				{
+					cwd = "/";
+				}
 
+				break;
+			case "/":
+				cwd = "/";
+				break;
 
-            //if it is end of ls command add directory entry
-            if (lastLs)
-            {
-                AddDirectory(cwd, lines);
-            }
+			default:
 
-            if (line.StartsWith("$ cd"))
-            {
-                lastLs = false;
+				if (cwd != "/")
+				{
+					cwd += "/";
+				}
 
-                var changeTo = line[5..];
-                switch (changeTo)
-                {
-                    case "..":
-                        cwd = cwd[..cwd.LastIndexOf("/")];
-                        if (!cwd.StartsWith("/"))
-                        {
-                            cwd = "/";
-                        }
+				cwd += changeTo;
+				break;
+		}
+	}
 
-                        continue;
-                    case "/":
-                        cwd = "/";
-                        continue;
-                    default:
+	private void AddDirectory(string name, IEnumerable<string> lsOutput)
+	{
+		long directChildrenSize = 0;
 
-                        if (cwd != "/")
-                        {
-                            cwd += "/";
-                        }
+		foreach (var entry in lsOutput)
+		{
+			//for now skip directories
+			if (entry.StartsWith("dir"))
+			{
+				continue;
+			}
 
-                        cwd += changeTo;
+			directChildrenSize += int.Parse(entry.Split(" ").First());
+		}
 
-                        break;
-                }
-            }
+		Directories.Add(name, directChildrenSize);
+	}
 
-            if (line.StartsWith("$ ls"))
-            {
-                lastLs = true;
-                lines = new List<string>();
-            }
-        }
-
-        //if last command is ls
-        if (lastLs)
-        {
-            AddDirectory(cwd, lines);
-        }
-    }
-
-
-    private void AddDirectory(string name, IEnumerable<string> lsOutput)
-    {
-        long directChildrenSize = 0;
-
-        foreach (var entry in lsOutput)
-        {
-            //for now skip directories
-            if (entry.StartsWith("dir"))
-            {
-                continue;
-            }
-
-            directChildrenSize += int.Parse(entry.Split(" ").First());
-        }
-
-        Directories.Add(name, directChildrenSize);
-    }
-
-    public Dictionary<string, long> NestedDirectorySize()
-    {
-        var Sizes = Directories
-            .OrderByDescending(x => x.Key.Length)
-            .ToDictionary(x => x.Key, x => x.Value);
-
-        foreach (var (name, size) in Sizes)
-        {
-            Console.WriteLine(name);
-
-            if (name == "/")
-            {
-                continue;
-            }
-            
-            var newName = name[..name.LastIndexOf("/")];
-
-            if (newName == "")
-            {
-                newName = "/";
-            }
+	public Dictionary<string, long> NestedDirectorySize()
+	{
+		var sizes = Directories
+			.OrderByDescending(x => x.Key.Length)
+			.ToDictionary(x => x.Key, x => x.Value);
 
 
-            //recursively add
+		//go from deepest directories and add its size to parent directory
+		foreach (var (name, size) in sizes)
+		{
+			if (name == "/")
+			{
+				continue;
+			}
 
-            Console.WriteLine($"Add  {size} FROM {name} TO {newName}");
+			var newName = name[..name.LastIndexOf("/")];
 
-            Sizes[newName] += size;
-            
-        }
+			if (newName == "")
+			{
+				newName = "/";
+			}
 
-        return Sizes;
-    }
+			// Console.WriteLine($"Add  {size} FROM {name} TO {newName}");
+
+			sizes[newName] += size;
+		}
+
+		return sizes;
+	}
 }
