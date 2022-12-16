e1df5688c4c51fd152a615caf7addbc5eb0c2318
Jan Rada <31016933+ZelvaMan@users.noreply.github.com>
2022-12-16 09:15:23 +0100

    csharp 2022/12 part 1

---
+++ csharp/Year2022/Day12/Solution.cs
@@ -0,0 +1,126 @@
+﻿using System.Numerics;
+
+namespace Csharp.Year2022.Day12;
+
+public class Solution
+{
+	public static string Part1(string[] input)
+	{
+		var map = new HeighMap(input);
+
+		var start = map.FindPoints('S').First();
+		var end = map.FindPoints('E').First();
+
+		var distance = map.FindShortestPath(start, end);
+		return distance.ToString();
+	}
+
+	public static string Part2(string[] input)
+	{
+		return "NOT IMPLEMENTED";
+	}
+}
+
+public class HeighMap
+{
+	private char[][] map;
+	private int widht;
+	private int heigh;
+
+	public HeighMap(string[] input)
+	{
+		map = input.Select(x => x.ToCharArray()).ToArray();
+		widht = input[0].Length;
+		heigh = input.Length;
+	}
+
+	private Queue<(Vector2 point, int distance)> queue;
+	private HashSet<Vector2> visited;
+
+	public int FindShortestPath(Vector2 start, Vector2 end)
+	{
+		visited = new HashSet<Vector2>();
+		queue = new Queue<(Vector2 point, int distance)>();
+
+		queue.Enqueue((start, 0));
+		visited.Add(start);
+
+		while (queue.Any())
+		{
+			var current = queue.Dequeue();
+			Console.WriteLine(visited.Count);
+			Console.WriteLine(queue.Count);
+			Console.WriteLine(current);
+			//check if we found end
+			if (current.point == end)
+			{
+				return current.distance;
+			}
+
+
+
+			TryAddToQueue(getVal(current.point), current.point + new Vector2(0, 1), current.distance + 1);
+			TryAddToQueue(getVal(current.point), current.point + new Vector2(0, -1), current.distance + 1);
+			TryAddToQueue(getVal(current.point), current.point + new Vector2(1, 0), current.distance + 1);
+			TryAddToQueue(getVal(current.point), current.point + new Vector2(-1, 0), current.distance + 1);
+		}
+
+		return -1;
+	}
+
+	public void TryAddToQueue(char oldVal, Vector2 newVector, int newDistance)
+	{
+		if (visited.Contains(newVector))
+		{
+			return;
+		}
+
+		if (newVector.X < 0 || newVector.X >= widht || newVector.Y < 0 || newVector.Y >= heigh)
+		{
+			return;
+		}
+
+		var newValue = getVal(newVector);
+
+		Console.WriteLine($"{oldVal} => {newValue}");
+
+		if ((newValue - 1) == oldVal || newValue <= oldVal)
+		{
+			// Console.WriteLine($"adding point {newVector}");
+			queue.Enqueue((newVector, newDistance));
+			visited.Add(newVector);
+		}
+	}
+
+	private char getVal(Vector2 point)
+	{
+		if (point.X < 0 || point.X >= widht || point.Y < 0 || point.Y >= heigh)
+		{
+			return (char) 255;
+		}
+
+		var value = map[(int) point.Y][(int) point.X];
+
+		return value switch
+		{
+			'E' => 'z',
+			'S' => 'a',
+			_ => value
+		};
+	}
+
+	public List<Vector2> FindPoints(char ch)
+	{
+		var found = new List<Vector2>();
+		for (int x = 0; x < widht; x++)
+		{
+			for (int y = 0; y < heigh; y++)
+			{
+				if (map[y][x] == ch)
+					found.Add(new Vector2(x, y));
+			}
+		}
+
+		return found;
+	}
+}
