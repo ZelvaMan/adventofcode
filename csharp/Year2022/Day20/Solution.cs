d0d0b35e079ddaee3867a361b4cd90bf0b698034
Jan Rada <31016933+ZelvaMan@users.noreply.github.com>
2022-12-20 20:29:22 +0100

    csharp 2022/20 part 2 working on ti

--- csharp/Year2022/Day20/Solution.cs
+++ csharp/Year2022/Day20/Solution.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Numerics;
 
 namespace Csharp.Year2022.Day20;
 
@@ -6,25 +7,39 @@ public class Solution
 {
 	public static string Part1(string[] input)
 	{
-		var parsed = input.Select(int.Parse);
+		var parsed = input.Select(BigInteger.Parse);
 		var Mixer = new Mixer(parsed);
 
 		Mixer.Mix();
 
 		var numbers = Mixer.Numbers.Take(3001).ToArray();
-		var result = ReadVal(numbers,1000) + ReadVal(numbers,2000) + ReadVal(numbers,3000);
+		var result = ReadVal(numbers, 1000) + ReadVal(numbers, 2000) + ReadVal(numbers, 3000);
 
 		return result.ToString();
 	}
 
-	private static int ReadVal(Element [] numbers, int index)
+	private static BigInteger ReadVal(Element[] numbers, BigInteger index)
 	{
-		return numbers[index % numbers.Length].value;
+		return numbers[(long) index % numbers.Length].value;
 	}
-	
+
 	public static string Part2(string[] input)
 	{
-		return "NOT IMPLEMENTED";
+		var parsed = input.Select(BigInteger.Parse).Select(x => x * 811_589_153);
+
+		var Mixer = new Mixer(parsed);
+		// Mixer.PrintList();
+		// Console.WriteLine("PART 2 CORE START");
+		for (BigInteger i = 0; i < 10; i++)
+		{
+			Mixer.Mix();
+			// Mixer.PrintList();
+		}
+
+		var numbers = Mixer.Numbers.Take(3001).ToArray();
+		var result = ReadVal(numbers, 1000) + ReadVal(numbers, 2000) + ReadVal(numbers, 3000);
+
+		return result.ToString();
 	}
 }
 
@@ -32,12 +47,12 @@ class Mixer
 {
 	public List<Element> Numbers;
 
-	public Mixer(IEnumerable<int> input)
+	public Mixer(IEnumerable<BigInteger> input)
 	{
 		CreateLinkedList(input);
 	}
 
-	private void CreateLinkedList(IEnumerable<int> input)
+	private void CreateLinkedList(IEnumerable<BigInteger> input)
 	{
 		Numbers = new List<Element>();
 
@@ -58,7 +73,7 @@ class Mixer
 		Numbers.First().left = Numbers.Last();
 		Numbers.Last().Right = Numbers.First();
 	}
-	
+
 
 	public void Mix()
 	{
@@ -66,13 +81,13 @@ class Mixer
 
 		foreach (var cur in Numbers)
 		{
-			Console.WriteLine($"DO MOVE FOR {cur.value}");
+			// Console.WriteLine($"DO MOVE FOR {cur.value}");
 			//move right
 			if (cur.value == 0) continue;
 
-			if (Math.Sign(cur.value) == 1)
+			if (Math.Sign((long) cur.value) == 1)
 			{
-				for (int i = 0; i < cur.value % (Numbers.Count - 1); i++)
+				for (BigInteger i = 0; i < cur.value % (Numbers.Count - 1); i++)
 				{
 					// Console.WriteLine("move one to the right of" + cur.Right.value);
 					cur.MoveRight();
@@ -80,17 +95,13 @@ class Mixer
 			}
 			else
 			{
-				for (int i = 0; i < Math.Abs(cur.value % (Numbers.Count - 1)); i++)
+				for (BigInteger i = 0; i < Math.Abs((long) (cur.value % (Numbers.Count - 1))); i++)
 				{
 					// Console.WriteLine("move one to the left of" + cur.left.value);
 					cur.MoveLeft();
 				}
 			}
-
-			BuildList();
 		}
-
-		Numbers = BuildList();
 	}
 
 	public List<Element> BuildList()
@@ -102,21 +113,29 @@ class Mixer
 		do
 		{
 			List.Add(current);
-			// Console.Write(current.value + ", ");
 			current = current.Right;
 		} while (current != head);
 
-		// Console.Write("\n");
 		return List;
+	}
+
+	public void PrintList()
+	{
+		foreach (var element in BuildList())
+		{
+			Console.Write(element.value + ", ");
+		}
+
+		Console.Write("\n");
 	}
 }
 
 internal class Element
 {
 	public Element left, Right;
-	public readonly int value;
+	public readonly BigInteger value;
 
-	public Element(Element left, int value)
+	public Element(Element left, BigInteger value)
 	{
 		this.left = left;
 		this.value = value;
